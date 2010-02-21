2e3edc4b06c976cc6cd65ef67ea87e46a0513d14
giorgio.tino@gmail.com <giorgio.tino@gmail.com@a4d420c8-1886-11df-8aaa-0dc69161900f>
2010-02-21 17:53:39 +0000

    Update issue 20 Just a very basic (i.e. dummy) audio manager class, needed by the game initialization routine.

---
+++ trunk/src/Audio/AudioManager.cpp
@@ -0,0 +1,48 @@
+#include "..\include\Audio\AudioManager.h"
+
+using namespace WyvernsAssault;
+
+AudioManager::AudioManager()
+{
+	//
+	// TODO Constructor
+	//
+}
+
+AudioManager::~AudioManager()
+{
+	//
+	// TODO Destructor
+	//
+	finalize();
+}
+
+/** Initialize the audio manager */
+void AudioManager::initialize()
+{
+	//
+	// TODO Initialize
+	//
+}
+
+/** Finalize the audio manager */
+void AudioManager::finalize()
+{
+	//
+	// TODO Finalize
+	//
+}
+
+void AudioManager::loadResources()
+{
+	//
+	// TODO Load resources
+	//
+}
+
+void AudioManager::unloadResources()
+{
+	//
+	// TODO Unload resources
+	//
+}
