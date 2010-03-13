d631c4c652dead28f303b842bef2102ed800c9ff
giorgio.tino <giorgio.tino@gmail.com>
2010-03-13 16:08:00 +0000

    Update issue 2 + Data, Logic, Physics, Entity(s) managers added. Still empty but good for the big picture

---
+++ src/Entity/Item/ItemManager.cpp
@@ -0,0 +1,17 @@
+#include "..\..\..\include\Entity\Item\ItemManager.h"
+
+using namespace WyvernsAssault;
+
+ItemManager::ItemManager()
+{
+	//
+	// TODO Constructor logic HERE
+	//
+}
+
+ItemManager::~ItemManager()
+{
+	//
+	// TODO Distructor logic HERE
+	//
+}
---
+++ src/Logic/LogicManager.cpp
@@ -0,0 +1,17 @@
+#include "..\..\include\Logic\LogicManager.h"
+
+using namespace WyvernsAssault;
+
+LogicManager::LogicManager()
+{
+	//
+	// TODO Constructor logic HERE
+	//
+}
+
+LogicManager::~LogicManager()
+{
+	//
+	// TODO Distructor logic HERE
+	//
+}
