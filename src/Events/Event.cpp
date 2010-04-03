2f2e08d89371bc11a537da40aa4f101f95f2a511
giorgio.tino <giorgio.tino@gmail.com>
2010-04-03 12:18:25 +0000

    Update issue 23 + Basic events system structure

---
+++ src/Events/Event.cpp
@@ -0,0 +1,17 @@
+#include "..\include\Events\Event.h"
+
+using namespace WyvernsAssault;
+
+Event::Event()
+{
+	//
+	// TODO Constructor
+	//
+}
+
+Event::~Event()
+{
+	//
+	// TODO Destructor
+	//
+}
