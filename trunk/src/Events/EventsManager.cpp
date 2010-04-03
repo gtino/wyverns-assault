3a8c03111d225815b3dada75c219d39b8d1de797
giorgio.tino@gmail.com <giorgio.tino@gmail.com@a4d420c8-1886-11df-8aaa-0dc69161900f>
2010-04-03 12:18:25 +0000

    Update issue 23 + Basic events system structure

---
+++ trunk/src/Events/EventsManager.cpp
@@ -0,0 +1,17 @@
+#include "..\include\Events\EventsManager.h"
+
+using namespace WyvernsAssault;
+
+EventsManager::EventsManager()
+{
+	//
+	// TODO Constructor
+	//
+}
+
+EventsManager::~EventsManager()
+{
+	//
+	// TODO Destructor
+	//
+}
