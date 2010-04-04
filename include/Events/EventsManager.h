df142d7c31daf108897706d5858d3abf54bd73e6
giorgio.tino <giorgio.tino@gmail.com>
2010-04-04 13:35:34 +0000

    Update issue 23 + More work on the events system

--- include/Events/EventsManager.h
+++ include/Events/EventsManager.h
@@ -42,6 +42,46 @@ namespace WyvernsAssault
 	public:
 		EventsManager();
 		~EventsManager();
+
+	public:
+		/// Delete all items from the event queue and
+		/// event handlers map
+		void clear();
+		/// Add an event to the queue
+		/// @param evt event to enqueue
+		void addEvent(EventPtr evt);
+		/// Process all events
+		void dispatchEvents();
+		/// Get the highest-priority event on the queue
+		/// @return highest-priority event on the queue
+		EventPtr peekEvent() const;
+		/// Remove the highest-priority event from the queue, and
+		/// return it.
+		/// @return removed event
+		EventPtr removeEvent();
+
+		/// Add a new event handler for the given event type
+		/// @param handler	functor object containing the instance that will handle the event 
+		///					and the callback method it'll invoke
+		/// @param evtType  type of the event that will be handled
+		void registerHandler(EventHandlerPtr handler,EventTypes evtType);
+
+
+		/// Delete an event handler for the given event type
+		/// @param handler  functor object to remove
+		/// @param evtType  type of the event
+		void unregisterHandler(EventHandlerPtr handler,EventTypes evtType);
+
+	private:
+		/** Priority queue to hold the events posted by the game objects or subsystems. */
+		EventQueue mEventQueue;
+		/// Map containing the registered event handlers. For each type of event there is a list linked to it
+		/// so the events may be quickly dispatched only to the instances interested on them.
+		/// For instance, say we have two event types: CAUSE_DAMAGE and RECEIVE_DAMAGE
+		/// A particular game object wants to call its member function "onCauseDamage" any time he hits something
+		/// Then, the map entry for CAUSE_DAMAGE will contain at least one event handler object linking the instance
+		/// that will react to the event and the callback method (the function onCauseDamage).		
+		EventHandlerMap mEventHandlers;
 	};
 }
 
--- src/Events/Event.cpp
+++ src/Events/Event.cpp
@@ -42,3 +42,17 @@ void Event::setData(EventData data)
 {
 	mData = data;
 }
+
+// -------------------------------
+bool EventComparator::operator() (EventPtr& evt1, EventPtr& evt2)
+{
+	if (!evt1.get()) return true;
+	if (!evt2.get()) return false;
+	// The more or less standard convention for a priority queue
+	// assigns more priority to values with lower numbers.
+	// However, the STL implementation does the opposite ¬_¬.
+	// To stick with the more familiar convention, the comparison operator
+	// used for this method is > instead of <, so that the queue ordering 
+	// gets reversed.
+	return evt1->getPriority()>evt2->getPriority();
+}
