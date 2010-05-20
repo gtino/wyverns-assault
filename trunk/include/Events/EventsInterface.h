004dea8cc0f9784e1682755ae725fe71aa82d21b
giorgio.tino@gmail.com <giorgio.tino@gmail.com@a4d420c8-1886-11df-8aaa-0dc69161900f>
2010-05-20 08:07:34 +0000

    + Initial commit for an EventsInterface

---
+++ trunk/include/Events/EventsInterface.h
@@ -0,0 +1,44 @@
+/*
+-----------------------------------------------------------------------------
+Wyverns' Assault 3D Videgame.
+Copyright (C) 2010  Giorgio Tino, Javier Soto Huesa, Jordi Carreras Ribot, 
+					Marc Serena, Elm Oliver Torres
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-----------------------------------------------------------------------------
+*/
+#ifndef __EVENTS_INTERFACE_H_
+#define __EVENTS_INTERFACE_H_
+
+#include "Event.h"
+
+namespace WyvernsAssault
+{
+	class EventsInterface
+	{
+	public:
+		// Register event handlers
+		virtual void registerHandlers() = 0;
+		// Unregister handlers
+		virtual void unregisterHandlers() = 0;
+		// Raise an event
+		virtual void raiseEvent(const EventPtr e) = 0;
+		// Handle an event
+		virtual void handleEvent(const EventPtr e) = 0;
+	};
+}
+
+#endif // __EVENTS_INTERFACE_H_
