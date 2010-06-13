45fe77fc8a5944aa54c806deb69e1f0d1f6c7eb3
javiersotohuesa <javiersotohuesa@a4d420c8-1886-11df-8aaa-0dc69161900f>
2010-06-13 17:07:25 +0000

    + Added gaussian blur compositor logic to code. ~ Modified DoF effect for use gaussian blur. ~ Changed g-buffer listener location to GraphicsManager. Added register gaussian blur compositor logic to GraphicsManager too.

--- trunk/include/States/PlayState.h
+++ trunk/include/States/PlayState.h
@@ -40,8 +40,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <SdkTrays.h>
 #include <ParticleUniversePlugin.h>
 
-#include <GBufferSchemeHandler.h>
-
 namespace WyvernsAssault
 {
 	/**
