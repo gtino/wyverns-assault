dd66fa1276645b0ca1e235e7a9d0e960aeff2ab9
javiersotohuesa <javiersotohuesa@a4d420c8-1886-11df-8aaa-0dc69161900f>
2010-09-17 15:35:41 +0000

    + Added new Credits and Splash screens (wide and normal aspect ratio).

--- trunk/src/States/CreditsState.cpp
+++ trunk/src/States/CreditsState.cpp
@@ -38,7 +38,12 @@ void CreditsState::load()
 	mGuiScreen = mGuiManager->createScreen(GuiScreenId::CreditsGui, "CreditsScreen");
 	
 	GuiBackgroundPtr guiBackground = GuiBackgroundPtr(new GuiBackground());
-	guiBackground->setImage("Credits.png","CreditsBackground","General");
+
+	// Common aspect ratio is 4/3
+	if( mWindow->getViewport(0)->getCamera()->getAspectRatio() > 1.34 )
+		guiBackground->setImage("wide/Credits.png","CreditsBackground","General");
+	else
+		guiBackground->setImage("normal/Credits.png","CreditsBackground","General");
 
 	mGuiScreen->setBackground(guiBackground);
 
