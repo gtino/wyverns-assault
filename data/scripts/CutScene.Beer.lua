2371d0200ebc40c659dde474bd98e30f12e4f115
giorgio.tino <giorgio.tino@gmail.com>
2010-09-30 07:38:34 +0000

    ~ Disable compositors when user skips scene

--- data/scripts/CutScene.Beer.lua
+++ data/scripts/CutScene.Beer.lua
@@ -102,6 +102,9 @@ function playBeerCutScene(elapsedTime)
 		Enemy.enable(); -- Resume enemy animations
 		Item.enable(); -- Resume items animation
 		
+		-- This is in case the user skips the cut scene
+		PostProcess.disableCompositor("MotionBlur"); -- Disable compositor
+		
 		CutScene.reset();		
 		finished = true; -- Scene has ended
 	end
